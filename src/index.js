fbe3cc97a0cbf10c349c238e31ee3d645b66878b
natalliabirytskaya <natalliabirytskaya@gmail.com>
2017-09-26 13:25:38 +0200

    Solution 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    arr[i] = bracketsConfig[i].join('');
+  };
+  let strCheck = '';
+  while (strCheck != str ) {
+    for (let i = 0; i < arr.length; i++) {
+      if (str.includes(arr[i])) {
+        strCheck = str;
+        str = str.replace(arr[i],'');
+      } 
+      else
+        strCheck = str;
+    }
+  }
+  return str.length === 0;
 }
